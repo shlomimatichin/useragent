1d55a31c354d0a49d1919438401f408e68ec1349
Shlomi Matichin <shlomomatichin@gmail.com>
2015-11-06 19:53:06 +0200

    Removed forced import from xojoc.pw

--- parse.go
+++ parse.go
@@ -14,7 +14,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 // Package useragent parses a user agent string.
-package useragent // import "xojoc.pw/useragent"
+package useragent
 
 import (
 	"fmt"
